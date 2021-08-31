0623f6c6f0f4d9e2396521eed1058f704becb9ae
Codeissimple <45950648+Codeissimple@users.noreply.github.com>
2021-08-31 00:09:38 +0000

    update

--- main.cpp
+++ main.cpp
@@ -10,4 +10,30 @@ int main() {
   Rectangle den;
 
   cout << "What is the kitchen's length?";
+  cin >> number;
+  kitchen.setLength(number);
+  cout << "What is the kitchen's width?";
+  kitchen.setWidth(number);
+
+  cout << "What is the bedroom's length?";
+  cin >> number;
+  bedroom.setLength(number);
+  cout << "What is the kitchen's width?";
+  cin >> number;
+  bedroom.setWidth(number);
+
+  cout << "What is the den's length?";
+  cin >> number;
+  den.setLength(number);
+  cout << "What is the den's width?";
+  cin >> number;
+  den.setWidth(number);
+
+  totalArea = kitchen.getArea()
+ + bedroom.getArea() + den.getArea();
+
+ cout << "The total area of the three rooms is " << totalArea << endl;
+
+ return 0;
+
 }
