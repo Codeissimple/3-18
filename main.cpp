1a9987782bc493feb468ae4b1f9155ffcdd14a9e
Codeissimple <45950648+Codeissimple@users.noreply.github.com>
2021-08-31 00:04:36 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+  double number;
+  double totalArea;
+  Rectangle kitchen;
+  Rectangle bedroom;
+  Rectangle den;
+
+  cout << "What is the kitchen's length?";
+}
